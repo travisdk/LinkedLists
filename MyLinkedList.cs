eabf568b18362d53d4a1f5eb00db63400515416a
Brian Andersen <brianmanden@gmail.com>
2023-10-01 08:50:11 +0200

    Revert "ADD Kommentarer til metoden Add(object data)"
    This reverts commit ef316f7cd7a487e2da8ebe411a60a107fa62dafd.

--- MyLinkedList.cs
+++ MyLinkedList.cs
@@ -42,9 +42,6 @@
 
         public void Add(object data)
         {
-            // Hvis Add(Count, data); bliver kørt som den første kommando vil det så virke ?
-            // Count har ikke eksplicit fået sat en default værdi i klassen.
-            // Det er muligt at int automatisk får tildelt tallet 0 (det mener jeg faktisk den gør)
             Add(Count, data);
         }
 
